e8279c0f83da14c953f7108a3700ec550244633a
greggiek <greg@bargainmoulding.com>
2026-09-01 08:36:58 -0400

    Support signed inventory CSV values

--- v2/inventory-values.js
+++ v2/inventory-values.js
@@ -13,6 +13,6 @@
       available: available === undefined || available === null ? rawOnHand - rawCommitted : number(available)
     };
   };
-  const forecastUsable = (onHand, committed) => Math.max(number(onHand) - number(committed), 0);
-  return { number, signedInventory, forecastUsable };
+  const forecastUsable = (onHand, committed) => Math.max(number(onHand) - number(committed), 0);\n  const csvCell = value => { const text=String(value ?? ''); return /[\",\\n]/.test(text) ? '\"'+text.replaceAll('\"','\"\"')+'\"' : text; };
+  return { number, signedInventory, forecastUsable, csvCell };
 });
